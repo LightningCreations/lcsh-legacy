43f546c444d655399546729a13c810b13b43fda0
Connor Horman <chorman64@gmail.com>
2020-05-06 18:56:36 -0400

    Do some lcjrt work, and some codefixing in lcrust

---
+++ cmake/cmake_symlink.cmake
@@ -0,0 +1,6 @@
+function(add_symlink name file_or_target)
+    if(TARGET ${file_or_target})
+        set()
+    endif()
+
+endfunction()
---
+++ src/main.c
@@ -0,0 +1,13 @@
+//
+// Created by chorm on 2020-05-04.
+//
+
+enum{
+    LCSH_NATIVE = 0,
+    LCSH_BASH = 1,
+    LCSH_SH_ONLY = 2
+};
+
+int main(int argc,char** argv){
+    int mode = LCSH_NATIVE;
+}
